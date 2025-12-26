112760dabd651a521185f69094ce345e19f4864e
rohitkr130593 <rohitkr130593@gmail.com>
2025-12-26 17:14:50 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# oryam-admin
+
---
+++ bootstrap/app.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function ($middleware) {
+    $middleware->web(append: [
+        \Illuminate\Session\Middleware\StartSession::class,
+        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
+    ]);
+})
+
+    ->withExceptions(function (Exceptions $exceptions): void {
+        //
+    })->create();
